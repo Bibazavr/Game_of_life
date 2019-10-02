e7e500b5001cccd23677202ca5817e9d74c503f7
Kirill <51177449+Bibazavr@users.noreply.github.com>
2019-10-02 14:18:45 +0300

    Add files via upload

--- game_of_life.py
+++ game_of_life.py
@@ -2,23 +2,21 @@ f = open('input.txt', 'r')
 first_stage = []
 row, column = 5, 5
 for line in f:
-    first_stage.append(line.split())
+    first_stage.append(list(''.join(line.split())))
 
 next_stage = []
-
-
-def find_live(stage, row, elem,):
+print('first_stage')
+for i in first_stage:
+    print(i)
+def find_live(stage, number_row, number_column):
     live = 0
-    for i in stage:
-        print(i)
-    print(elem)
-    if stage[-row + 1][elem] == 'x':
+    if stage[number_row + 1][number_column] == 'X':
         live += 1
-    if stage[-row - 1][elem] == 'x':
+    if stage[number_row - 1][number_column] == 'X':
         live += 1
-    if stage[-row][elem - 1] == 'x':
+    if stage[number_row][number_column - 1] == 'X':
         live += 1
-    if stage[-row][elem + 1] == 'x':
+    if stage[number_row][number_column + 1] == 'X':
         live += 1
     return live
 
@@ -28,11 +26,12 @@ for i in range(row):
     for j in range(column):
         count_live = find_live(first_stage, i, j)
         print(count_live)
-        if first_stage[i][j] == 'x' and 2 < count_live <= 3:
-            next_stage.append(i)
+        if (first_stage[i][j] == 'X' and 2 < count_live <= 3) or (first_stage[i][j] == '.' and count_live == 3):
+            next_row.append("X")
+        else:
+            next_row.append('.')
 
     next_stage.append(next_row)
-
+print('next_stage')
 for row in next_stage:
-    for j in row:
-        print(j)
+    print(row)
